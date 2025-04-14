ec8c746ceed9c8512dea00b6912c2e3305571074
Carlos Gabriel Barbosa Paz <carlosgabriel_paz@hotmail.com>
2025-04-13 21:14:06 -0300

    commit 2

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListarOrgaos from './components/Orgaos/ListarOrgaos';
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Licitações</h1>
+      <ListarOrgaos />
+      {/* Adicione outros componentes/rotas conforme desejar */}
+    </div>
+  );
+}
+
+export default App;
